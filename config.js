9a7112c45d31d9701674a141aed98d493b5c9893
quanpapi <168348965+quanpapi@users.noreply.github.com>
2024-04-28 21:04:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "hé lô em ",
     introTitle: 'Diệu Huyên',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introDesc: `Nếu ah đã vô tình làm em buồn , thì cho ah xl nhá`,
+    btnIntro: '^^HiHi ko sao ợ ^^',
+    title: 'Phải chẳng ah đã thích e ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng ah đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 8 tỉ người ko bt sao ah lại chỉ thích mỗi e <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Tuy ah ít nói ko bt nhắn gì nhưng ah đối vs e là th lòng  <33333333',
+    mess: 'e biết mà 🥰. ko sao ợ 😘😘',
+    messDesc: 'Hãy chờ ah nhá.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
