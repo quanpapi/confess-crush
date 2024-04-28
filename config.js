0f0e4f08260b7f0afbfe0d80fa76432ca06d274c
quanpapi <168348965+quanpapi@users.noreply.github.com>
2024-04-28 21:00:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "hé lô em ",
+    introTitle: 'Diệu Huyên',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
